6a600b1c28de8cb529c76172e1477210ef693c89
ScOrP <alex_77_90@mail.ru>
2024-08-28 23:23:42 +0500

    Промежуточные правки

--- DM_draft/exam/api/serializers.py
+++ DM_draft/exam/api/serializers.py
@@ -12,6 +12,7 @@ from users.api.serializers import CompanySerializer
 
 class ExamSerializer(serializers.ModelSerializer):
     company = serializers.CharField(source='cc.name', read_only=True)
+
     # cc = CompanySerializer()
 
     class Meta:
@@ -21,10 +22,30 @@ class ExamSerializer(serializers.ModelSerializer):
 
 
 class AddInternSerializer(serializers.ModelSerializer):
-
     class Meta:
         model = Exam
         fields = ['date_exam', 'name_intern', 'cc']
+
+    def validate(self, data):
+        # Создаем экземпляр модели с данными
+        instance = Exam(**data)
+        print(instance)
+        try:
+            # Вызываем full_clean() для проверки ошибок модели
+            instance.full_clean()
+
+        except serializers.ValidationError as e:
+            print(e.message_dict)
+            # Обрабатываем ошибки и возвращаем их как ошибки сериализатора
+            raise serializers.ValidationError(e.message_dict)
+
+        return data
+
+
+class EditInternSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Exam
+        fields = ['id', 'date_exam', 'name_intern', 'cc']
 
     def validate(self, data):
         # Создаем экземпляр модели с данными
--- DM_draft/exam/api/view.py
+++ DM_draft/exam/api/view.py
@@ -1,6 +1,6 @@
 from rest_framework import viewsets
 
-from .serializers import ExamSerializer, AddInternSerializer
+from .serializers import ExamSerializer, AddInternSerializer, EditInternSerializer
 from ..models import Exam
 
 
@@ -11,10 +11,18 @@ class ExamView(viewsets.ModelViewSet):
 
 class AddIntersViewSet(viewsets.ModelViewSet):
     queryset = Exam.objects.all()
-    serializer_class = AddInternSerializer
+
+    serializer_list = {
+        'POST': AddInternSerializer,
+        'PUT': EditInternSerializer
+    }
+
+    def get_serializer_class(self):
+        if self.serializer_list.get(self.request.method):
+            return self.serializer_list[self.request.method]
+        else:
+            return AddInternSerializer
 
     def perform_create(self, serializer):
         print("Данные, переданные для создания:", self.request.data)
         serializer.save()
-
-
